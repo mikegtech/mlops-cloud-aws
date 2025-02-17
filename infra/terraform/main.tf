a54a0531eafe4a07756053a8659300e11af7f4d4
Michael Gatewood <mike@mikegtech.com>
2025-02-17 11:44:57 -0600

    changes for s3 bucket

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -52,100 +52,138 @@ resource "aws_service_discovery_http_namespace" "this" {
   description = "CloudMap namespace for ${local.name}"
 }
 
-module "mlops-data-catalog" {
+module "multi_inventory_configurations_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "~> 4.0"
 
-  bucket_prefix = "${local.name}-data-"
-  bucket        = "catalog-directory"
-  is_directory_bucket = false
+  bucket = "${local.name}-data-catalog"
 
-  availability_zone_id = data.aws_availability_zones.available.zone_ids[1]
-  server_side_encryption_configuration = {
-    rule = {
-      bucket_key_enabled = true # required for directory buckets
-      apply_server_side_encryption_by_default = {
-        kms_master_key_id = aws_kms_key.objects.arn
-        sse_algorithm     = "aws:kms"
+  force_destroy = true
+
+  attach_policy                       = true
+  attach_inventory_destination_policy = true
+  inventory_self_source_destination   = true
+
+  versioning = {
+    status     = true
+    mfa_delete = false
+  }
+
+  inventory_configuration = {
+
+    # Same source and destination buckets
+    daily = {
+      included_object_versions = "Current"
+      destination = {
+        format = "CSV"
+        encryption = {
+          encryption_type = "sse_kms"
+          kms_key_id      = module.kms.key_arn
+        }
       }
+      filter = {
+        prefix = "documents/"
+      }
+      frequency = "Daily"
+    }
+
+    weekly = {
+      included_object_versions = "All"
+      destination = {
+        format = "CSV"
+      }
+      frequency = "Weekly"
+    }
+
+    # Different destination bucket
+    destination_other = {
+      included_object_versions = "All"
+      destination = {
+        bucket_arn = module.inventory_destination_bucket.s3_bucket_arn
+        format     = "Parquet"
+        encryption = {
+          encryption_type = "sse_s3"
+        }
+      }
+      frequency       = "Weekly"
+      optional_fields = ["Size", "EncryptionStatus", "StorageClass", "ChecksumAlgorithm"]
+    }
+
+    # Different source bucket
+    source_other = {
+      included_object_versions = "Current"
+      bucket                   = module.inventory_source_bucket.s3_bucket_id
+      destination = {
+        format = "ORC"
+        encryption = {
+          encryption_type = "sse_s3"
+        }
+      }
+      frequency = "Daily"
     }
   }
-  lifecycle_rule = [
+}
+
+resource "random_pet" "this" {
+  length = 2
+}
+
+# https://docs.aws.amazon.com/AmazonS3/latest/userguide/configure-inventory.html#configure-inventory-kms-key-policy
+module "kms" {
+  source  = "terraform-aws-modules/kms/aws"
+  version = "~> 2.0"
+
+  description             = "Key example for Inventory S3 destination encyrption"
+  deletion_window_in_days = 7
+  key_statements = [
     {
-      id     = "test"
-      status = "Enabled"
-      expiration = {
-        days = 7
-      }
-    },
-    {
-      id     = "logs"
-      status = "Enabled"
-      expiration = {
-        days = 5
-      }
-      filter = {
-        prefix                = "logs/"
-        object_size_less_than = 10
-      }
-    },
-    {
-      id     = "other"
-      status = "Enabled"
-      expiration = {
-        days = 2
-      }
-      filter = {
-        prefix = "other/"
-      }
+      sid = "s3InventoryPolicy"
+      actions = [
+        "kms:GenerateDataKey",
+      ]
+      resources = ["*"]
+
+      principals = [
+        {
+          type        = "Service"
+          identifiers = ["s3.amazonaws.com"]
+        }
+      ]
+
+      conditions = [
+        {
+          test     = "StringEquals"
+          variable = "aws:SourceAccount"
+          values = [
+            data.aws_caller_identity.current.id,
+          ]
+        },
+        {
+          test     = "ArnLike"
+          variable = "aws:SourceARN"
+          values = [
+            module.inventory_source_bucket.s3_bucket_arn,
+            module.multi_inventory_configurations_bucket.s3_bucket_arn
+          ]
+        }
+      ]
     }
   ]
-  attach_policy = true
-  policy        = data.aws_iam_policy_document.bucket_policy.json
 }
 
-resource "aws_kms_key" "objects" {
-  description             = "KMS key is used to encrypt bucket objects"
-  deletion_window_in_days = 7
+module "inventory_destination_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket                              = "inventory-destination-${random_pet.this.id}"
+  force_destroy                       = true
+  attach_policy                       = true
+  attach_inventory_destination_policy = true
+  inventory_source_bucket_arn         = module.multi_inventory_configurations_bucket.s3_bucket_arn
+  inventory_source_account_id         = data.aws_caller_identity.current.id
 }
 
-data "aws_iam_policy_document" "bucket_policy" {
+module "inventory_source_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
 
-  statement {
-    sid    = "ReadWriteAccess"
-    effect = "Allow"
-
-    actions = [
-      "s3express:CreateSession",
-    ]
-
-    resources = [module.mlops-data-catalog.s3_directory_bucket_arn]
-
-    principals {
-      identifiers = [data.aws_caller_identity.current.account_id]
-      type        = "AWS"
-    }
-  }
-
-  statement {
-    sid    = "ReadOnlyAccess"
-    effect = "Allow"
-
-    actions = [
-      "s3express:CreateSession",
-    ]
-
-    resources = [module.mlops-data-catalog.s3_directory_bucket_arn]
-
-    principals {
-      identifiers = [data.aws_caller_identity.current.account_id]
-      type        = "AWS"
-    }
-
-    condition {
-      test     = "StringEquals"
-      values   = ["ReadOnly"]
-      variable = "s3express:SessionMode"
-    }
-  }
+  bucket        = "inventory-source-${random_pet.this.id}"
+  force_destroy = true
 }
